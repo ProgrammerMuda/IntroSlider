9df893585b4fab43beba74480051dc9272aa3445
raga <ragamuthahari@gmail.com>
2019-01-08 09:11:28 +0700

    Initial commit

---
+++ app/src/main/java/slider/com/co/learnintroslider/HomeActivity.java
@@ -0,0 +1,36 @@
+package slider.com.co.learnintroslider;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.btn_play_again).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // We normally won't show the welcome slider again in real app
+                // but this is for testing
+                pref prefManager = new pref(getApplicationContext());
+
+                // make first time launch TRUE
+                prefManager.setFirstTimeLaunch(true);
+
+                startActivity(new Intent(HomeActivity.this, IntroSlider.class));
+                finish();
+            }
+        });
+    }
+
+}
+
